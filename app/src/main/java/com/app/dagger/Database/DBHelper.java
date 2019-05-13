6bef2ba344eaa8ba9f5baa69d0f4b9d9a22407a1
Mujahid Khan <mkhan9047@gmail.com>
2019-05-13 14:59:27 +0600

    intital commit

---
+++ app/src/main/java/com/app/dagger/Database/DBHelper.java
@@ -0,0 +1,66 @@
+package com.app.dagger.Database;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.app.dagger.Anotations.ApplicationContext;
+import com.app.dagger.Anotations.DatabaseInfo;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class DBHelper extends SQLiteOpenHelper {
+
+    //USER TABLE
+    public static final String USER_TABLE_NAME = "users";
+    public static final String USER_COLUMN_USER_ID = "id";
+    public static final String USER_COLUMN_USER_NAME = "usr_name";
+    public static final String USER_COLUMN_USER_ADDRESS = "usr_add";
+    public static final String USER_COLUMN_USER_CREATED_AT = "created_at";
+    public static final String USER_COLUMN_USER_UPDATED_AT = "updated_at";
+
+    @Inject
+    public DBHelper(@ApplicationContext Context context,
+                    @DatabaseInfo String dbName,
+                    @DatabaseInfo Integer version) {
+        super(context, dbName, null, version);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private void tableCreateStatements(SQLiteDatabase db) {
+        try {
+            db.execSQL(
+                    "CREATE TABLE IF NOT EXISTS "
+                            + USER_TABLE_NAME + "("
+                            + USER_COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            + USER_COLUMN_USER_NAME + " VARCHAR(20), "
+                            + USER_COLUMN_USER_ADDRESS + " VARCHAR(50), "
+                            + USER_COLUMN_USER_CREATED_AT + " VARCHAR(10) DEFAULT " + getCurrentTimeStamp() + ", "
+                            + USER_COLUMN_USER_UPDATED_AT + " VARCHAR(10) DEFAULT " + getCurrentTimeStamp() + ")"
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getCurrentTimeStamp() {
+        return String.valueOf(System.currentTimeMillis() / 1000);
+    }
+}
